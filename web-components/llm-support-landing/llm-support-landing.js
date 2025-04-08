7073bb8d90a4f99a823d1ac532cfdaf289239f15
VictorCondurat <conduratvictor@yahoo.com>
2025-04-08 14:05:51 +0300

    feat: add application structure boilerplate

---
+++ web-components/llm-support-landing/llm-support-landing.js
@@ -0,0 +1,19 @@
+export class LlmSupportLanding {
+    constructor(element, invalidate) {
+        this.element = element;
+        this.invalidate = invalidate;
+        this.pageName = ""
+        this.invalidate();
+    }
+
+    async beforeRender() {
+
+  
+    }
+
+
+    async afterRender() {
+
+    }
+}
+  
